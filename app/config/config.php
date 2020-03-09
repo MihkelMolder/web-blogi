7c70c0861a416322b75ea11d7fafa11621e2fd46
MihkelMolder <37287994+MihkelMolder@users.noreply.github.com>
2020-03-09 23:15:50 +0200

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+// rakendusega esotud konstandid
+define("URLROOT", "http://localhost/web");
+define("APPROOT", dirname(dirname(__FILE__)));
+define("SITENAME", "IS web");
+
+
+//db
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'web');
